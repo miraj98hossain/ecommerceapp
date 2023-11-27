4978ef4225aaabc1f843af9fb5186cc8772dccc0
Miraj <miraj09hossain@gmail.com>
2023-11-27 13:58:32 +0600

    constant colors created

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+import 'package:ecommerceapp/myApp.dart';
 import 'package:ecommerceapp/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-    );
-  }
 }
---
+++ lib/myApp.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerceapp/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+    );
+  }
+}
