d803fc79660078e8fab25c08c447f180902a7b21
Miraj <miraj09hossain@gmail.com>
2023-11-29 12:41:08 +0600

    navigation customized

--- lib/bottom_navigation_bar.dart
+++ lib/bottom_navigation_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/buttom_navigationbar_controller.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class ButtomNavigationBAr extends StatelessWidget {
   const ButtomNavigationBAr({super.key});
@@ -9,11 +10,22 @@ class ButtomNavigationBAr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(ButtomNavigationbarController());
+    final dark = THelperFunctions.isDarkMode(context);
     return SafeArea(
       child: Scaffold(
         bottomNavigationBar: Obx(
           () => NavigationBar(
+              backgroundColor: dark
+                  ? Colors.white
+                  : const Color.fromARGB(255, 218, 218, 218),
+              animationDuration: const Duration(seconds: 1),
+              indicatorColor: dark
+                  ? Colors.white.withOpacity(0.1)
+                  : Colors.black.withOpacity(0.1),
+              labelBehavior:
+                  NavigationDestinationLabelBehavior.onlyShowSelected,
               height: 80,
+              elevation: 0,
               selectedIndex: controller.currentIndex.value,
               onDestinationSelected: (value) {
                 controller.currentIndex.value = value;
